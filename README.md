16c618a92fdb9aaab286d4183738c243ce2a63f5
Caique Lins <caique.c.lins@gmail.com>
2021-03-15 12:31:46 -0300

    Adição do projeto

---
+++ README.md
@@ -0,0 +1,52 @@
+<h1 align="center">
+  <img alt="move.it" title="move.it" src=".github/logo.png" />
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-executar">Como executar</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=8257E5&labelColor=000000">
+
+ <img src="https://img.shields.io/static/v1?label=NLW&message=04&color=8257E5&labelColor=000000" alt="NLW 04" />
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Happy" src=".github/moveit.png" width="100%">
+</p>
+
+## ✨ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://reactjs.org)
+- [Next.js](https://nextjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## 💻 Projeto
+
+O move.it é um app que une a técnica de Pomodoro com a realização de exercícios físicos para quem passa muito tempo na frente do computador.
+
+## 🚀 Como executar
+
+- Clone o repositório
+- Vá até a pasta do arquivo
+- Instale as dependências com `yarn` (na pasta do arquivo, digite **yarn**)
+- Inicie o servidor com `yarn dev` (na pasta do arquivo digite **yarn dev**)
+
+Agora você pode acessar [`localhost:3000`](http://localhost:3000) do seu navegador, para utilizar a aplicação.
+
+## 📄 Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Feito com ♥ by Caique Lins
---
+++ src/components/Countdown.tsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContext';
+import style from '../styles/components/Countdown.module.css';
+
+
+
+export function Countdown() {
+    const { minutes, seconds, hasFinished, isActive, startCountdown, resetCountdown } = useContext(CountdownContext)
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+    
+
+    return (
+        <div>
+            <div className={style.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+
+            { hasFinished ? (
+                <button disabled className={style.countdownButton}>
+                    Ciclo encerrado
+                </button>
+            ) : (
+                    <>
+                        { isActive ? (
+                            <button type="button" className={`${style.countdownButton} ${style.countdownButtonActive}`} onClick={resetCountdown}>
+                                Abandonar ciclo
+                            </button>
+                        ) : (
+                                <button type="button" className={style.countdownButton} onClick={startCountdown}>
+                                    Iniciar um ciclo
+                                </button>
+                            )}
+                    </>
+                )}
+        </div>
+    );
+}
